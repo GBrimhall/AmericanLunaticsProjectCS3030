cc4f9a0312e68a9dc1fe7e75a333061676c9917d
Micheal Brewer <mbrewerramirez@mail.weber.edu>
2017-04-06 12:29:00 -0600

    layed out def

---
+++ create_report.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 student1 <student1@CS3030_87>
+#
+
+
+#===========================================================================
+#
+#       File: create_report.py 
+#
+#       Usage: ./create_report.py
+#
+#   Description:
+#
+#       Options: ---
+#       ReqOPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Micheal Brewer (), mbrewerramirez@mail.weber.edu
+#        AUTHOR:
+#        AUTHOR:
+#  ORGANIZATION:
+#       CREATED: 03/03/2017 14:27
+#      REVISION:  ---
+#===========================================================================
+
+import sys
+
+def Date(beg_date, end_date):
+    """
+    Translate the date taken in
+    ARGS:
+        beg_date(YYYYMMDD), end_date(YYYYMMDD)
+    returns:
+        YYYY-MM-DD hh:mm
+    """
+
+
+
+    #bad input return -1
+
+def fix_len(data):
+    """
+    Format of the return
+    """
+    #sort data
+    #print(format)
+    #return 0
+
+def query_data(formatted_date):
+    """
+    query the db
+    """
+
+
+    # test exist Not return -2
+
+
+
+
+
+
+#Main Function
+def main():
+    """
+    Test Function
+    """
+
+
+
+if __name__=="__main__":
+    # Call Main
+    main()
+
+    exit(0)
