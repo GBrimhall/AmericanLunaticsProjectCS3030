4f5431ad51981cb9efea3923530327cd4402cf0d
Micheal Brewer <mbrewerramirez@mail.weber.edu>
2017-04-06 20:46:10 -0600

    query is ready to pull all info needed.

--- create_report.py
+++ create_report.py
@@ -32,11 +32,7 @@ import sqlite3
 import time
 import datetime
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> c75422d9b8cdd4aa8e515afb71e0bcb147153953
 
 def Date(beg_date, end_date):
     """
@@ -53,26 +49,25 @@ def Date(beg_date, end_date):
     #bad input return -1
 
 
-def query_data(formatted_date):
+def query_data():
     """
     query the db
     """
-<<<<<<< HEAD
-        
-=======
     
     #Connect to DB
-    conn = sqlite.connect('hw8SQLite.db'):
-        if (conn):
-            print("Connected to DB")
-    
-
+    conn = sqlite3.connect('hw8SQLite.db')
+    if (conn):
+        print("Connected to DB")
+    c = conn.cursor()
+    c.execute('SELECT T.trans_id, T.trans_date, T.card_num, TL.Qty, TL.amt, P.prod_desc, T.total FROM trans T INNER JOIN trans_line TL ON T.trans_ID=TL.trans_ID INNER JOIN products P ON TL.prod_num=P.prod_num WHERE trans_date BETWEEN "2017-04-02 00:00" AND "2017-04-02 23:59" ORDER BY T.trans_ID')
+    recs = c.fetchall()
+    for row in recs:
+        print(row)
     #close the cursor
 
     #close connection
     conn.close()
 
->>>>>>> c75422d9b8cdd4aa8e515afb71e0bcb147153953
 
 
     # test exist Not return -2
@@ -94,7 +89,7 @@ def main():
     """
     Test Function
     """
-
+    query_data()
 
 
 
