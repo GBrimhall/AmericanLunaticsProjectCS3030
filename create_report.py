1dfd43db7c86d684d17dce2a81d55423a4190188
Gavin Brimhall <gavinbrimhall@mail.weber.edu>
2017-04-12 16:49:04 -0600

    Updated the .py file and the .sh file

--- create_report.py
+++ create_report.py
@@ -6,9 +6,9 @@
 #
 
 
-#===========================================================================
+# ===========================================================================
 #
-#       File: create_report.py 
+#       File: create_report.py
 #
 #       Usage: ./create_report.py
 #
@@ -21,21 +21,22 @@
 #         NOTES: ---
 #        AUTHOR: Micheal Brewer (), mbrewerramirez@mail.weber.edu
 #        AUTHOR: Cayden Allen ,  caydenallen@mail.weber.edu
+#        AUTHOR: Gavin Brimhall ,  gavinbrimhall@mail.weber.edu
 #  ORGANIZATION:
 #       CREATED: 03/03/2017 14:27
 #      REVISION:  ---
-#===========================================================================
+# ===========================================================================
 
 import sys
-import os
+# import os
 import zipfile
 import sqlite3
 
-#beg_date = sys.argv[1]
-#end_date = sys.argv[2]
-eCode = ' '
-#bDate = ""
-#eDate = ""
+# beg_date = sys.argv[1]
+# end_date = sys.argv[2]
+# eCode = ' '
+# bDate = ""
+# eDate = ""
 
 
 def help():
@@ -44,7 +45,6 @@ def help():
     """
     print("Usage is: " + sys.argv[0] + "<begDate>  <endDate>")
     exit(1)
-
 
 
 def verify(date):
@@ -66,128 +66,134 @@ def convert_date():
     """
     beg_date = sys.argv[1]
     end_date = sys.argv[2]
-    global eDate 
-    global bDate 
-    
+    global eDate
+    global bDate
+
     if verify(beg_date) and verify(end_date):
-        bDate = beg_date[:4]+'-'+beg_date[4:6]+'-'+beg_date[6:]+ ' 00:00:00'
-        eDate = end_date[:4]+'-'+end_date[4:6]+'-'+end_date[6:]+ ' 23:59:59'
-        #print("after conversion", bDate, eDate)
+        bDate = beg_date[:4] + '-' + beg_date[4:6] + '-' + beg_date[6:]
+        + '00:00:00'
+        eDate = end_date[:4] + '-' + end_date[4:6] + '-' + end_date[6:]
+        + '23:59:59'
+        # print("after conversion", bDate, eDate)
     else:
         exit(1)
-        eCode = '1'
-    
+        # eCode = '1'
 
 
 def query():
     """
     Pulls the arguments when the script is called
     """
-    
-    #connect to db
-    conn = sqlite3.connect('hw8SQLite.db')
-    c=conn.cursor()
 
-    #variables managing report data
-    report =""
+    # connect to db
+    conn = sqlite3.connect('hw8SQLite.db')
+    c = conn.cursor()
+
+    # variables managing report data
+    report = ""
     transID = -1
     total = 0
     fill = '00000000          '
     count = 0
 
-    #Query database
-    for row in c.execute("SELECT trans_id, trans_date, card_num, qty, amt, prod_desc, total FROM trans LEFT JOIN trans_line USING (trans_id) LEFT JOIN products USING (prod_num) WHERE trans_date > '" + bDate +"' AND trans_date < '" + eDate +"'"):
+    # Query database
+    for row in c.execute("SELECT trans_id, trans_date, card_num, qty, amt, prod_desc, total FROM trans LEFT JOIN trans_line USING (trans_id) LEFT JOIN products USING (prod_num) WHERE trans_date > '" + bDate + "' AND trans_date < '" + eDate + "'"):
         if row[5] is None:
-            desc =''
+            desc = ''
         else:
             desc = row[5]
-        #Determine if same transaction  
+        # Determine if same transaction
         if transID != row[0]:
-            #Generate report Data for new transaction
+            # Generate report Data for new transaction
             if report == "":
-                report = '{s:{c}>{n}}'.format(s=str(row[0]),n=5,c='0')      
+                report = '{s:{c}>{n}}'.format(s=str(row[0]), n=5, c='0')
             else:
-                #fills in blank products in report
-                while (count < 3):                   
-                    report += fill      
-                    count += 1                                                                                                                      
-                
-                #adds total tothe new transaction
-                report += '{s:{c}>{n}}'.format(s=str('{:.2f}'.format(total)).replace('.',''),n=6,c='0')
-                report += '\n{s:{c}>{n}}'.format(s=str(row[0]),n=5,c='0')
-                            
-            #Adds first product to report         
-            transDate = row[1].replace(':','')
-            transDate = transDate.replace('-','')
-            transDate = transDate.replace(' ','')
-            transDate = str(transDate[:12])     
-            report += transDate + str(row[2][-6:]) 
+                # fills in blank products in report
+                while (count < 3):
+                    report += fill
+                    count += 1
+
+                # adds total tothe new transaction
+                report += '{s:{c}>{n}}'.format(s=str('{:.2f}'.format(total))
+                                               .replace('.', ''), n=6, c='0')
+                report += '\n{s:{c}>{n}}'.format(s=str(row[0]), n=5, c='0')
+
+            # Adds first product to report
+            transDate = row[1].replace(':', '')
+            transDate = transDate.replace('-', '')
+            transDate = transDate.replace(' ', '')
+            transDate = str(transDate[:12])
+            report += transDate + str(row[2][-6:])
             if row[3] is None:
-                report += '{s:{c}>{n}}'.format(s='0',n=2,c='0')
+                report += '{s:{c}>{n}}'.format(s='0', n=2, c='0')
             else:
-                report += '{s:{c}>{n}}'.format(s=str(int(row[3])),n=2,c='0')
+                report += '{s:{c}>{n}}'.format(s=str(int(row[3])), n=2, c='0')
             if row[4] is None:
                 price = 0
             else:
                 price = '{:.2f}'.format(row[4])
-            price = '{s:{c}>{n}}'.format(s=str(price).replace('.',''),n=6,c='0')
-            report += price + '{s:{c}<{n}}'.format(s=desc,n=10,c=' ')
-            transID=row[0]  
+                price = '{s:{c}>{n}}'.format(s=str(price).replace('.', ''),
+                                             n=6, c='0')
+            report += price + '{s:{c}<{n}}'.format(s=desc, n=10, c=' ')
+            transID = row[0]
             total = row[6]
             count = 1
         else:
-            #Adds remaining products
-            report += '{s:{c}>{n}}'.format(s=str(int(row[3])),n=2,c='0')
+            # Adds remaining products
+            report += '{s:{c}>{n}}'.format(s=str(int(row[3])), n=2, c='0')
             price = '{:.2f}'.format(row[4])
-            price = '{s:{c}>{n}}'.format(s=str(price).replace('.',''),n=6,c='0')
-            report += price + '{s:{c}<{n}}'.format(s=desc,n=10,c=' ')
+            price = '{s:{c}>{n}}'.format(s=str(price).replace('.', ''),
+                                         n=6, c='0')
+            report += price + '{s:{c}<{n}}'.format(s=desc, n=10, c=' ')
             total += row[6]
             count += 1
-        #Print last entry of report
-    #Determine if data in query
 
-    if report == "":     
-        eCode = '-2'
-        #exit -2
+    # Print last entry of report
+    # Determine if data in query
+    if report == "":
+        # eCode = '-2'
+        # exit -2
         exit(2)
     else:
         while (count < 3):
             report += fill
             count += 1
-        eCode = '0'
-        #exit = 0
-        #adds total then then the new transaction
-        report += '{s:{c}>{n}}'.format(s=str('{:.2f}'.format(total)).replace('.',''),n=6,c='0')
+        # eCode = '0'
+        exit(0)
+        # adds total then then the new transaction
+        report += '{s:{c}>{n}}'.format(s=str('{:.2f}'.format(total))
+                                       .replace('.', ''), n=6, c='0')
     print(report)
 
-    #Create file for report
-    text_file = open ('company_trans_'+ sys.argv[1] + '_'+ sys.argv[2]+'.dat', 'w')
+    # Create file for report
+    text_file = open('company_trans_' + sys.argv[1] + '_' + sys.argv[2] +
+                     '.dat', 'w')
     text_file.write('%s' % report)
     text_file.close()
-    
-    return(eCode)
+
+    # return(eCode)
 
 
-def zip_file(): 
-    #Zip file
-    with zipfile.ZipFile('company_trans_'+ sys.argv[1] + '_'+ sys.argv[2]+'.dat.zip', 'w') as myzip:
-        myzip.write('company_trans_'+ sys.argv[1] + '_'+ sys.argv[2]+'.dat')
+def zip_file():
+    # Zip file
+    with zipfile.ZipFile('company_trans_' + sys.argv[1] + '_' +
+                         sys.argv[2] + '.dat.zip', 'w') as myzip:
+        myzip.write('company_trans_' + sys.argv[1] + '_' + sys.argv[2] + '.dat')
 
-    #print('exit code would be ' + str(eCode))
+    # print('exit code would be ' + str(eCode))
 
 
-
-#Main Function
+# Main Function
 def main():
     """
     Test Function
     """
     convert_date()
     query()
-    #zip_file()
+    # zip_file()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # Call Main
     main()
 
--- run_report.sh
+++ run_report.sh
@@ -8,23 +8,23 @@
 
 
 
-#!/bin/bash - 
+#!/bin/bash -
 #===============================================================================
 #
 #          FILE: run_report.sh
-# 
-#         USAGE: ./run_report.sh 
-# 
-#   DESCRIPTION: 
-# 
+#
+#         USAGE: ./run_report.sh
+#
+#   DESCRIPTION:
+#
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
 #        AUTHOR: Micheal Brewer (), mbrewerramirez@mail.weber.edu
+#		 AUTHOR: Gavin Brimhall, gavinbrimhall@mail.weber.edu
 #		 AUTHOR:
-#		 AUTHOR:
-#  ORGANIZATION: 
+#  ORGANIZATION:
 #       CREATED: 04/05/2017 21:14
 #      REVISION:  ---
 #===============================================================================
@@ -50,8 +50,8 @@ fi
 
 # Getopts while loop
 while getopts ":f:t:e:u:p:" opt
-do 
-	case $opt in
+do
+    case $opt in
 		f) begDate=$OPTARG #first opt
 			;;
 		t) endDate=$OPTARG #second opt
@@ -71,7 +71,7 @@ done
 
 #IF statement to test if parameters are set. -z checks to see if value is null
 if [[ -z $begDate ]] || [[ -z $endDate ]]
-then 
+then
 	usage
 elif [[ -z $email ]] || [[ -z $user ]]
 then
@@ -90,7 +90,7 @@ fi
 # FTP transfer
 ftp(){
 	File = "company_trans_${begDate}'_'${endDate}.dat"
-	ftp -n "137.190.19.87" <<END_SCRIPT 
+	ftp -n "137.190.19.87" <<END_SCRIPT
 	quote USER $user
 	quote PASS $passwd
 	binary
@@ -103,35 +103,28 @@ exit 0
 
 # Do wrapper
 #create_report($begDate $endDate)
-#exit_code = 
+#exit_code =
+
+#CHANGE THE PYTHON command
 python3 ./create_report.py $begDate $endDate
+ret=$?
 
-
-
-	#CHANGE THE PYTHON command
-case exit_code in
-	0)
+if [[ $ret == 0 ]]
 		date=`date +%Y_%m_%d_%H:%M`
 		echo "Zipping up new file"
-		python -c'import create_report.py; create_report.zip_file()'  #attempt to call python function for zip
+		python -c 'import create_report.py; create_report.zip_file()'  #attempt to call python function for zip
 		echo "Transferring to FTP"
 		ftp
 		echo "Sending confirmation to email: $email"
 		` mail -s "Successfully transfered file ($HOST) " $email <<< "Successfully created a transaction report from $begDate to $endDate"`
-		;;	
-	1)
+
+else if [[ $ret == 1 ]]
 		echo "Sending error -1 to email: $email"
 		` mail -s "The create_report program exit with code -1 " $email <<< "Bad Input parameters begin date: $begDate and end date: $endDate"`
-		;;
-	2)
+
+else if [[ $ret == 2 ]]
 		echo "Sending error -2 to email: $email"
 		` mail -s "The create_report program exit with code -2 " $email <<< "No Transaction available from begin date: $begDate to end date: $endDate"`
-	;;
-esac
-
-
-
 
 
 exit 0
-
